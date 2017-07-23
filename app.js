bbdba50b21b633c721038b06a8fee881e3ece300
rivermanvr <vrgithub@riversconsulting.com>
2017-07-23 14:53:23 -0400

    setup main menu concept

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
 const express = require( 'express' );
 const app = express();
 const path = require( 'path' );
+const swig = require( 'swig' );
+
+app.set('view engine', 'html');
+app.engine('html', swig.renderFile);
+swig.setDefaults({ cache: false});
+
+app.use('/css', express.static(path.join(__dirname, 'css')));
+app.use('/vendor', express.static(path.join(__dirname, 'node_modules')));
+
+app.get('/', (req, res, next) => {
+  res.render('index');
+})
 
 module.exports = app;
